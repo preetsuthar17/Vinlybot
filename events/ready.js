94182b35d72be3f4bee453ace2f3d09d042af108
pritulx <prituyt@gmail.com>
2021-07-09 19:00:26 +0530

    Added automatic changing status!

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,22 @@
-const client = require("discord.js");
+// const client = require("discord.js");
+const client = require('../index') 
+const statuses = [
+  "rex help",
+  "rex invite to invite me",
+  "Rule breakers",
+  "How pritu coded me. Hmmmmmmmmmm",
+  "Hii",
+  "I am Rexx",
+  "Powerfull moderation",
+  `Over ${client.guilds.cache.size} Servers!`,
+  `Over ${client.users.cache.size} Servers!`
+]
 
 module.exports.run = (client) => {
   console.log(`✅ Logged on as ${client.user.username}`);
-  client.user.setActivity("rex help");
+  setInterval(() => {
+    const index = Math.floor(Math.random() * (statuses.length - 1) + 1);
+    const mainActivity = statuses[index]
+    client.user.setActivity(mainActivity)
+  }, 10000);
 };
